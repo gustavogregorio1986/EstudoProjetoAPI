c394a3914bb2d7fb745fef50d4539c183891bd5f
guga56 <gustavogregorio086@gmail.com>
2022-08-10 12:53:04 -0300

    criar as demais interface

---
+++ EstudoProjetoAPI.Data/Repositorio/Interface/ILoginRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using EstudoProjetoAPI.Dominio.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstudoProjetoAPI.Data.Repositorio.Interface
+{
+    public interface ILoginRepositorio : IGenericRepositorio<Login>
+    {
+    }
+}
