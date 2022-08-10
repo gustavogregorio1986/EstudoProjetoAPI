6e5b044f097eba60f6687410b60cfae7b0163923
guga56 <gustavogregorio086@gmail.com>
2022-08-10 12:13:43 -0300

    criar o genericRepositorio

---
+++ EstudoProjetoAPI.Data/Repositorio/Interface/IGenericRepositorio.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstudoProjetoAPI.Data.Repositorio.Interface
+{
+    public interface IGenericRepositorio<T> where T : class
+    {
+        void Adicionar(T entidade);
+
+        List<T> ObterTodos();
+
+        List<T> Obter(Expression<Func<T, bool>> predicate);
+
+        T Buscar(params object[] Key);
+
+        T Primeiro(Expression<Func<T, bool>> predicate);
+
+        void Atualizar(T entidade);
+
+        void Deletar(int i);
+
+        void Commit();
+    }
+}
