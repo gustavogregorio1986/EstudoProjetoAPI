68dde813535d37db84d5a502fae8f4c88cf54bee
guga56 <gustavogregorio086@gmail.com>
2022-08-10 14:48:57 -0300

    criarção das classes herdndo das interfaces

---
+++ EstudoProjetoAPI.Data/Repositorio/AdministradorRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using EstudoProjetoAPI.Data.Repositorio.Interface;
+using EstudoProjetoAPI.Dominio.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstudoProjetoAPI.Data.Repositorio
+{
+    public class AdministradorRepositorio : GenericRepositorio<Administrador>, IAdministradorRepositorio
+    {
+        public IEnumerable<Administrador> Buscar(object id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
